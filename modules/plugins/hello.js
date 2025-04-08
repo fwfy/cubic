ffe906c541f35031d19d3fa1ca32d65e6a1792ae
Aedan Ball <fwfy+codesign@fwfy.club>
2025-04-08 10:53:27 -0400

    refactor: remove weird testing command

--- modules/plugins/hello.js
+++ modules/plugins/hello.js
@@ -4,7 +4,6 @@ function init(env, cobj) {
     ctrl = cobj;
     ctrl.registerCommand("hello", commandHelloWorld, "What good would a bot be without a nice, old-fashioned 'Hello, world!' command?", ["hi", "greetings"]);
     ctrl.registerCommand("debug", commandDebug, `Replies with some information about the initiating message.`);
-    ctrl.registerCommand("burger", commandBurger, `borgoir`);
 }
 
 function commandHelloWorld(cmd, args, msg) {
@@ -13,10 +12,6 @@ function commandHelloWorld(cmd, args, msg) {
 
 function commandDebug(cmd, args, msg) {
     msg.reply(`Information about this message:\nArguments: ${args.join(', ')}\nYour ID: ${msg.author.id}\nBot Owner: ${msg.author.is_owner}\nProvider Module: ${msg.source}`);
-}
-
-function commandBurger(cmd, args, msg) {
-    msg.reply(`Eat Borgir`, [`https://files.fwfy.club/files/button.png`]);
 }
 
 module.exports = init;
