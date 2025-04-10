698cde3929285885278b4d6a211a86327575ab4c
Aedan Ball <fwfy+codesign@fwfy.club>
2025-04-10 05:05:50 -0400

    feat: add support for buttons

--- modules/providers/discord.js
+++ modules/providers/discord.js
@@ -1,5 +1,5 @@
 const { Provider } = require('../../provider_api.js');
-const { Client, Events, GatewayIntentBits, REST, Routes, Partials, AttachmentBuilder } = require('discord.js');
+const { Client, Events, GatewayIntentBits, REST, Routes, Partials, AttachmentBuilder, ActionRowBuilder, ButtonStyle, ButtonBuilder} = require('discord.js');
 const { log, LEVEL, MIN_LEVEL } = require('../../logger.js');
 const axios = require('axios');
 
@@ -18,6 +18,10 @@ class DiscordProvider extends Provider {
     interactionHandler(interaction) {
         if(interaction.isChatInputCommand()) {
             ctrl.dispatchCommand(this.transformMessage(interaction, "command"));
+        } else if(interaction.isButton()) {
+            let transformed = this.transformMessage(interaction, "button");
+            ctrl.dispatchCommand(transformed);
+            // interaction.deferUpdate();
         }
     }
     messageCreateHandler(message) {
@@ -47,10 +51,10 @@ class DiscordProvider extends Provider {
         let user = msg.author || msg.user;
         let obj = {
             source: this.platform_name,
-            reply: (text, attachments) => this.replyToMessage(this.platform_name, msg, text, attachments),
+            reply: (text, attachments, buttons) => this.replyToMessage(this.platform_name, msg, text, attachments, buttons),
             message: {
                 type,
-                content: msg.content || msg.commandName,
+                content: msg.content || msg.commandName || msg.customId,
                 attachments: false
             },
             author: {
@@ -62,10 +66,29 @@ class DiscordProvider extends Provider {
         }
         return obj;
     }
-    async replyToMessage(source, msg, text, attachments=[]) { // everything to do with attachments is 1 for 1 copied from popcord because i cba to actually think right now
+    async replyToMessage(source, msg, text, attachments=[], buttons=[]) { // (almost) everything to do with attachments is 1 for 1 copied from popcord because i cba to actually think right now
         if(source !== this.platform_name) return; // TODO: is silently failing the best option here?
         let options = {};
         options.content = text;
+        if(buttons.length > 0) {
+            const actionRow = new ActionRowBuilder();
+            for(const btn of buttons) {
+                if(!btn.label) {
+                    log("Skipping button on message because it lacks a btn.label!", LEVEL.WARN);
+                    continue;
+                }
+                if(!btn.action) {
+                    log("Skipping button on message because it lacks a btn.action!", LEVEL.WARN);
+                    continue;
+                }
+                const builder = new ButtonBuilder()
+                    .setCustomId(btn.action)
+                    .setLabel(btn.label)
+                    .setStyle(ButtonStyle.Primary);
+                actionRow.addComponents(builder);
+            }
+            options.components = [actionRow];
+        }
         if(attachments.length > 0) {
             let attachment;
             //text = null;
@@ -73,6 +96,7 @@ class DiscordProvider extends Provider {
             let file = attachments[0];
             switch(typeof file) {
                 case "string": // probably a URL to an external file, so we download it first using axios
+                    log(`Downloading external attachment from "${file}".`, LEVEL.DEBUG);
 				    let data = await axios(file, { responseType: "arraybuffer" });
 				    attachment = Buffer.from(data.data, "binary");
 				    break;
@@ -88,6 +112,7 @@ class DiscordProvider extends Provider {
     async postPluginInit() {
         let commands = [];
         for(const command of ctrl.commands) {
+            if(command[1].description == "[HIDDEN]") continue;
             commands.push({
                 name: command[0],
                 description: command[1].description
